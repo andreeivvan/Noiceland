f913c9122fc0ecad03de41f59373a4485269033d
andreeivvan <webcontinue10@gmail.com>
2022-07-08 15:12:10 +0300

    noi

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('.top-nav__burger').click(function(event){
+        $('.top-nav__burger,.nav-menu--top-nav').toggleClass('active');
+        $('body').toggleClass('lock');
+    })
+})
+
+(function(d){
+    function c(k){return(d.cookie.match('(^|; )'+k+'=([^;]*)')||0)[2];}
+    var ua = navigator.userAgent,
+        ismobile = / mobile/i.test(ua),
+        mgecko = !!( / gecko/i.test(ua) && / firefox\//i.test(ua)),
+        wasmobile = c('wasmobile') === "was",
+        desktopvp = 'user-scalable=yes, width=1000, minimum-width=1000',
+        el;
+    if (ismobile && !wasmobile) {
+        d.cookie = "wasmobile=was";
+    }
+    else if (!ismobile && wasmobile) {
+        if (mgecko) {
+            el = d.createElement('meta');
+            el.setAttribute('content', desktopvp);
+            el.setAttribute('name', 'viewport');
+            d.getElementsByTagName('head')[0].appendChild(el);
+        }else{
+            d.getElementsByName('viewport')[0].setAttribute('content', desktopvp);
+        }
+    }
+}(document));
